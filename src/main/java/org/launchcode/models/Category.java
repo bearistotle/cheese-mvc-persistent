1c938025ea48bf4c1740b269f53cdfcc17544611
Brandon Walker <brandonwalker215@gmail.com>
2019-06-22 09:19:45 -0500

    "Finished studio part 2 w/ bonus mission"

--- src/main/java/org/launchcode/models/Category.java
+++ src/main/java/org/launchcode/models/Category.java
@@ -1,10 +1,10 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Category {
@@ -16,6 +16,16 @@ public class Category {
     @NotNull
     @Size(min=3, max=15, message="Name must be 3--15 characters.")
     private String name;
+
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private List<Cheese> cheeses = new ArrayList<>();
+    /* Declared as List<> because in Java it is a good idea to declare the most general type (highest in the hierarchy)
+    as possible. This is because Java creates a space in memory that can only be filled by objects of the type declared.
+    You can only put objects of that general type or a species of that type in the space in memory that is created. If
+    you were to declare this as an ArrayList, you could only switch it to another ArrayList or a subtype of ArrayList.
+    By declaring it as a List, you can switch it to a broader range of data types--List or any of its children/subtypes.
+     */
 
     public Category(){
 
@@ -35,6 +45,10 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
     }
 }
 
